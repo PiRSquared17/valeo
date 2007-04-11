c4e664fd5a28ee04386a087e66c9bfdbc8beaa97
leogregianin <leogregianin@4c97d7b0-052b-0410-ba41-5d02874a72a5>
2007-04-11 15:48:48 +0000

    yeah yeah, edit body message with ui!

--- libgmail/send_mail.py
+++ libgmail/send_mail.py
@@ -3,6 +3,8 @@
 
 from getpass import getpass
 from sys import exit, argv
+from Tkinter import Tk
+from gui import EditBoxWindow
 
 from libgmail import GmailAccount, GmailComposedMessage, GmailSendError, GmailLoginFailure
 
@@ -24,9 +26,12 @@ if __name__ == "__main__":
 
     gmailto = raw_input('Send to: ')
     gmailsubject = raw_input('Subject: ')
-    gmailbody = raw_input('Body message: ')
 
-    attach = raw_input('Send with attachment? Input filename or empty to continue: ')
+    tk = Tk()
+    app = EditBoxWindow(tk)
+    gmailbody = app.edit('Edit this...')
+    
+    attach = raw_input('\nSend with attachment? Input filename or empty to continue: ')
     print "\nPlease wait, send message..."
     if attach == '':
         gcm = GmailComposedMessage(gmailto, gmailsubject, gmailbody)
