d8d9a92b6c4e53079d96d3eac44fe904ddc5fdc0
leogregianin <leogregianin@4c97d7b0-052b-0410-ba41-5d02874a72a5>
2007-04-24 16:43:27 +0000

    google mail script 0.0.1 (python2.5)

---
+++ trunk/other/google mail script.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# lib
+from smtplib import SMTP
+from email.MIMEText import MIMEText
+from getpass import getpass
+from sys import argv, exit
+
+#except
+from smtplib import SMTPAuthenticationError
+from smtplib import SMTPRecipientsRefused
+
+# gui
+try:
+    from Tkinter import Tk
+    from gui import EditBoxWindow
+except ImportError:
+    print "install python-tkinter and gui.py"
+
+
+def sendmsg(fromg, to, subj):
+    tk = Tk()
+    app = EditBoxWindow(tk)
+    body = MIMEText(app.edit(u"Edit this...")
+    
+    fromg = body["From"] = argv[1]
+    to = body["To"] = argv[2]
+    subj = body["Subject"] = argv[3]
+    body["User-Agent"] = "Google mail script (Python2.5)"
+
+    server = SMTP("smtp.gmail.com", 587)
+    #server.set_debuglevel(0)
+    
+    server.ehlo(fromg)
+    server.starttls()
+    server.ehlo(fromg)
+    
+    try:
+        passwd = getpass()
+        server.login(fromg, passwd)
+    except SMTPAuthenticationError, exc:
+        print 'Login error: %s' % exc
+        exit()
+
+    try:
+        # todo: unicode error
+        server.sendmail(fromg, to, body.as_string())
+        server.quit()
+        print 'Email send ok!'
+    except SMTPRecipientsRefused, ex:
+        print 'Email send error: %s' % exc
+        exit()
+
+if __name__ == '__main__':
+    try:
+        sendmsg(argv[1], argv[2], argv[3])
+    except IndexError:
+        print 'run gmail-smtp.py from to subject'
