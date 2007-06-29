018fc909e6de45f9828e79f48d399f018b4ed303
leogregianin <leogregianin@4c97d7b0-052b-0410-ba41-5d02874a72a5>
2007-06-29 17:10:22 +0000

    0.1 final version for gnome linux

--- CommonsPictureOfTheDay/setup_gconf.py
+++ CommonsPictureOfTheDay/setup_gconf.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 
-## python setup.py install
-
 from distutils.core import setup
 
 setup(
-    name = 'Wallpaper-commons',
-    version = '0.0.1',
+    name = 'CommonsPictureOfTheDay',
+    version = '0.1',
     description = 'Put "picture of the day" in your Desktop wallpaper from Wikimedia Commons',
     author = 'Leonardo Gregianin',
     author_email = 'leogregianin@gmail.com',
-    url = 'http://code.google.com/p/wallpaper-commons',
-    download_url = 'http://code.google.com/p/wallpaper-commons/downloads/list',
+    url = 'http://code.google.com/p/valeo/wiki/CommonsPictureOfTheDay',
+    download_url = 'http://code.google.com/p/valeo/downloads/list',
     license = 'MIT',
     classifiers=[
           'Development Status :: 4 - Beta',
@@ -26,8 +24,12 @@ setup(
           ],
     
     packages = ['pywikipedia', 'pywikipedia.families', 'pywikipedia.userinterfaces'],
-    )    
+    )
 
+## python setup.py install
+
+# TODO:
 # create nautilus-python entry:
 #   desktop -> preferences -> sessions ->
-#      startup programs -> add -> /usr/lib/python2.5/site-packages/wallpaper-commons/pywikipedia/wallpaper-gconf.py
+#      startup programs -> add ->
+#          /usr/lib/python2.5/site-packages/CommonsPictureOfTheDay/pywikipedia/CommonsPictureOfTheDay-Gconf.py
