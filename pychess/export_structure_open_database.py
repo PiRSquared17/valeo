1eee2df5adb25aeba170efcce3c81d9ec306515d
leogregianin <leogregianin@4c97d7b0-052b-0410-ba41-5d02874a72a5>
2011-01-29 20:59:26 +0000

    test

--- pychess/export_structure_open_database.py
+++ pychess/export_structure_open_database.py
@@ -1,5 +1,4 @@
 import sqlite3, os
 con = sqlite3.connect('open.db')
 with open('openings.sql', 'w') as f:
-    for row in con.iterdump():
-        f.write('%s\n' % row)
+    for row in con.iterdump(): f.write('%s\n' % row)
